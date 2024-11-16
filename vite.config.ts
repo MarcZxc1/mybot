e942fe637928ddd71fe96c9e5b820c406fe19d82
marc <dagodemarcgeraldarante@gmail.com>
2024-11-16 22:23:41 +0800

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,10 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 import react from '@vitejs/plugin-react-swc';
 import mkcert from 'vite-plugin-mkcert';
 
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/reactjs-template',
+  base: '/mybot/',
   plugins: [
     // Allows using React dev server along with building a React application with Vite.
     // https://npmjs.com/package/@vitejs/plugin-react-swc
@@ -16,6 +17,7 @@ export default defineConfig({
     // Create a custom SSL certificate valid for the local machine.
     // https://www.npmjs.com/package/vite-plugin-mkcert
     mkcert(),
+    
   ],
   publicDir: './public',
   server: {
